c5020aa0f74d933918131ade137381dbaa46e3b2
Indi Rinearson <indi.rinearson@gmail.com>
2022-03-01 13:20:57 -0800

    initial commit

---
+++ config/firebase.ts
@@ -0,0 +1,37 @@
+import firebase from "firebase/compat/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+import "firebase/compat/auth";
+import Constants from "expo-constants";
+
+interface Config {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
+
+const firebaseConfig: Config = {
+  apiKey: Constants.manifest!.extra!.apiKey,
+  authDomain: Constants.manifest!.extra!.authDomain,
+  projectId: Constants.manifest!.extra!.projectId,
+  storageBucket: Constants.manifest!.extra!.storageBucket,
+  messagingSenderId: Constants.manifest!.extra!.messagingSenderId,
+  appId: Constants.manifest!.extra!.appId,
+  measurementId: Constants.manifest!.extra!.measurementId,
+};
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = getFirestore(app);
+export const auth = firebase.auth();
+export default db;
